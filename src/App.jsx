fe8f4541e8abb009a10e5abeccc37ad1b93bd6c3
Aman Verma <avman1998@gmail.com>
2023-10-01 19:35:42 +0530

    Implementation of RTK query

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import { useEffect, useState } from "react";
 import Product from "./Product";
 import Cart from "./Cart";
+import { useGetProductQuery } from "./redux/api";
 import "./App.css";
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const fetchData = async () => {
-    const res = await fetch("https://fakestoreapi.com/products");
-    const data = await res.json();
-    setProducts(data);
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // const [products, setProducts] = useState([]);
+  // const fetchData = async () => {
+  //   const res = await fetch("https://fakestoreapi.com/products");
+  //   const data = await res.json();
+  //   setProducts(data);
+  // };
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  const { data: products, error, isLoading } = useGetProductQuery();
+  if (error) return <h1>Error : {error}</h1>;
+
+  if (isLoading) return <h1>Loading......</h1>;
+
   return (
     <>
       {products?.map((item) => {
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { productApi } from "./api";
 import cartSlice from "./cartSlice";
 const store = configureStore({
   reducer: {
     cart: cartSlice,
+    [productApi.reducerPath]: productApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productApi.middleware),
 });
 export default store;
